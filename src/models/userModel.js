2c8c8afac87363f7574b97311879816dd263d569
Gifty Nkrumah <aksalnkrumah@gmail.com>
2020-12-04 23:17:55 +0000

    Initial commit

---
+++ src/models/userModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: [true, 'first name required']
+    },
+    
+    middlename: {
+        type: String
+    },
+
+    lastname: {
+        type: String,
+        required: [true, 'last name required']
+    },
+    
+    username: {
+        type: String,
+        minlength: [5, 'minimum username length is 5'],
+        maxlength: [16, 'maximum username length is 16'], 
+        unique: [true, 'username already taken'],
+        required: [true, 'username required'],
+        lowercase: true
+    },
+
+    email: {
+        type: String,
+        unique: [true, 'email already has an account'],
+        required: [true, 'email address required'],
+        lowercase: true
+    },
+
+    password: {
+        type: String,
+        minlength: [8, 'minimum password length is 8'],
+        required: [true, 'password required']
+    }
+})
+
+const User = mongoose.model('user', userSchema)
+
+module.exports = User
